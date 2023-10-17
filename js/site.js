4baedacc594c8fe93a7aa31013b068c2b9ce2a79
TJF71 <138714842+TJF71@users.noreply.github.com>
2023-10-16 22:25:19 -0400

    first commit to git

--- js/site.js
+++ js/site.js
@@ -1,14 +1,74 @@
-function displayMessage() {
-// get the text out of the input and assign it to a variable
-    let msg = document.getElementById('message').value;
 
+// get the user input to reverse
+function getValues() {
+    // get the text out of the input and assign it to a variable
+    let message = document.getElementById('userInput').value;
 
- Swal.fire(
-    {
-        backdrop: false,
-        title: 'APP NAME',
-        text: msg,  //replace this with that variable
+    // validate input and make sure its not empty
+    if (message.length == 0) {
+
+        Swal.fire(
+            {
+                backdrop: 'error',
+                backdrop: false,
+                text: 'Please enter a string to check'   //replace this with that variable
+            }
+        );
+
+        } else {
+
+        // pas the user input to reverseMessage function and assign it's return to a variable
+        let revMsg = reverseMessage(message);
+
+        //send the message adn reverse message to checkForPalindrome
+        let palindrome = checkForPalindrome(message, revMsg);
+
+            // give th results to display messare
+        displayResults(message, revMsg, palindrome);  
+        
+
     }
- );
+}
+
+// reverse the string
+function reverseMessage(input) {
+
+    // define return
+    let output = '';
+
+    for (let i = input.length - 1;  i >=0 ; i--) {
+        output += input[i];
+    }
+
+    return output;
 
 }
+
+
+// check to see if the strings match
+
+function checkForPalindrome(msg, revmsg){
+    if (msg == revmsg) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+function displayResults (msgEntered, revMsg, PalTrueFalse){
+
+    document.getElementById('msg').textContent = `Your message is ${msgEntered}`;
+    document.getElementById('msg').textContent = `Your reversed message is ${revMsg}`;
+    document.getElementById('msg').textContent = `It is ${PalTrueFalse} that your message is a palindrome`;
+
+
+
+    // document.getElementById('msg').textContent = `Your message in reverse is ${revMsg}`;
+
+
+    document.getElementById('alert').classList.remove('invisible');
+
+}
+
+// display the message
